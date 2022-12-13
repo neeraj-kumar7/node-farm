7697de1a4cc9f48bd1b4ab3f543594e3e5519632
neeraj <neerajpenumaka7@gmail.com>
2022-12-13 10:44:05 +0530

    using modules

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
+const replaceTemplate = require('./modules/replaceTemplate');
 //////////// FILE SYSTEM /////////////////////////////////////
 
 /*
@@ -40,21 +41,6 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
-
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-}
-
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
